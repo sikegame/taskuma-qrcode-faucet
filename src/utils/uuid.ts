c461d9f847ac37933948ddb4076c906d24978585
Shinsuke JJ Ikegame <jj@digitalnomad.jp>
2023-01-11 12:31:44 +0700

    Change uuid4 identifier

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,5 +1,15 @@
 import { v4 as uuidv4 } from "uuid";
 
+const SECTION_IDENTIFIER = "s";
+
+function getUuid4(): string {
+  /**
+   * Returns a UUID v4 string without hyphens.
+   */
+  return uuidv4().replace(/-/g, "");
+}
+
 export function generateQrCodeValue(): string {
-  return `taskuma://t/${uuidv4()}`;
+  const uuid = getUuid4();
+  return `taskuma://${SECTION_IDENTIFIER}/${uuid}`;
 }
